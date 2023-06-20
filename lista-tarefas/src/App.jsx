86ede841b841ad6b74f7ee9fe188d35a137da71c
Rafael Silva <rafael.sdasilva84@hotmail.com>
2023-06-19 22:22:37 -0300

    correção erros

--- lista-tarefas/src/App.jsx
+++ lista-tarefas/src/App.jsx
@@ -5,7 +5,7 @@ export default function App() {
   const tasks = ['Reunião Condominio Jardim Zona Sul', 'Vistoria Condominio Nonoai', 'Vistoria Condominio Petropólis','Vistoria Canoas'];
   return (
     <div>
-      <h1 className='text-center mt-5'>Título</h1>
+      <h1 className='text-center mt-5'>Lista de Tarefas</h1>
       <ToDo tasks={tasks}/>
     </div>
     
--- lista-tarefas/src/components/Task.jsx
+++ lista-tarefas/src/components/Task.jsx
@@ -1,5 +1,7 @@
-export default function Task ({task}) {
-  return <li className="list-group-item d-flex justify-content-between align-items-center">{task}
-  <button type='button' className="btn btn-primary btn-sm">Pendente</button>
+export default function Task({ task }) {
+
+  return <li className="list-group-item d-flex justify-content-between align-items-center">
+    {task}
+    <button type='button' className="btn btn-primary btn-sm">Pendente</button>
   </li>
 }
